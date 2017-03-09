d4d131f258ef6705d13c3b416ac3640074a3c31f
Kelvin Rosado <Zrelok@users.noreply.github.com>
2017-03-09 14:38:25 -0500

    Add files via upload

---
+++ Question.java
@@ -0,0 +1,20 @@
+//Should be renamed to Question Factory
+//Early prototype for now
+public class Question {
+	String[] qs;
+	String[] as;
+
+	public Question(){
+		qs = new String[1];
+		as = new String[4];
+		qs[0] = "What is 2 + 2?";
+		as[0] = "1";
+		as[1] = "2";
+		as[2] = "3";
+		as[3] = "4";
+	}
+	
+	
+	
+	
+}
