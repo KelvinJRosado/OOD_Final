b070623ef400f16a12edf103ea5f4936bb9fb5a9
Kelvin Rosado <Zrelok@users.noreply.github.com>
2017-03-15 22:45:59 -0400

    Add files via upload

---
+++ EnemyFiles/TestQuestions.java
@@ -0,0 +1,15 @@
+
+public class TestQuestions {
+
+	public static void main(String[] args) {
+		//Asks what the first letter is and provides choices
+		Question q = new Question("First letter of the alphabet?", "A", "Z", "D", "F");
+		//Asks for 2 + 2 and provides choices
+		Question q2 = new Question("2 + 2?", "4", "2", "22", "10");
+		
+		q.ask();
+		q2.ask();
+
+	}
+
+}
