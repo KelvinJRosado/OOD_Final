8813756a3a3105fdcaa55f2f992c05fdfd498021
Kelvin Rosado <Zrelok@users.noreply.github.com>
2017-03-30 14:58:41 -0400

    Update EasyBoss.java

--- EnemyFiles/EasyBoss.java
+++ EnemyFiles/EasyBoss.java
@@ -6,8 +6,13 @@ public class EasyBoss extends Boss{
 		setHp(30);//Set hp
 		
 		//Add questions here
-		questions.add(new Question("2 + 2?", "4", "2", "22", "10"));
-		questions.add(new Question("First letter of the alphabet?", "A", "Z", "D", "F"));
+		questions.add(new Question("A language which is close to that used within the computer is ", "Low-level language", "High-level language", "Assembly language", "None of the above"));
+		questions.add(new Question("The first generation of computers available was based on the bit micro processors. ", "8", "4", "16", "64"));
+		questions.add(new Question("The step-by- step instructions that solve a problem are called _____. ", "An algorithm", "A list", "A plan", "A sequential structure"));
+		questions.add(new Question("The operator used to get value at address stored in a pointer variable is ", "*", "&", "&&", "||"));
+		
+		
+		
 	}
 	
 	//Attack the player
