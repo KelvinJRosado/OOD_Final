67e36510cd0fbda2146016d0b7cc8f35e20833d8
Kelvin Rosado <Zrelok@users.noreply.github.com>
2017-04-06 14:31:15 -0400

    Add files via upload

---
+++ PlayGame.java
@@ -0,0 +1,12 @@
+
+public class PlayGame {
+
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+
+		Game g = new Game();
+		
+		g.play();
+
+	}
+
+}
